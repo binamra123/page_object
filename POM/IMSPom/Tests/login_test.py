343e8c70c5af6cf1f071e6696170adbd277c8c4b
binamraacharya <46298949+binamra123@users.noreply.github.com>
2024-03-17 21:22:36 +0545

    changes maded

--- POM/IMSPom/Tests/login_test.py
+++ POM/IMSPom/Tests/login_test.py
@@ -23,4 +23,61 @@ def test_login(setup):
     time.sleep(2)
     login_page.click_login()
     time.sleep(5)
+    assert driver.title == "Projects | Infrastructure Management System Stage"
+
+def test_invalogin(setup):
+    login_page = LoginPage(driver)
+    login_page.open_page(URLConfig.LOGIN_PAGE_URL)
+    login_page.enter_username("admin")
+    login_page.enter_password("abcd")
+    time.sleep(2)
+    login_page.click_login()
+    time.sleep(5)
+
+def test_invname(setup):
+    login_page = LoginPage(driver)
+    login_page.open_page(URLConfig.LOGIN_PAGE_URL)
+    login_page.enter_username("admin")
+    login_page.enter_password("password")
+    time.sleep(2)
+    login_page.click_login()
+    time.sleep(5)
+
+def test_invpass(setup):
+    login_page = LoginPage(driver)
+    login_page.open_page(URLConfig.LOGIN_PAGE_URL)
+    login_page.enter_username("admin.dhangadhi")
+    login_page.enter_password("abcd")
+    time.sleep(2)
+    login_page.click_login()
+    time.sleep(5)
+
+def test_remember(setup):
+    login_page = LoginPage(driver)
+    login_page.open_page(URLConfig.LOGIN_PAGE_URL)
+    login_page.enter_username("admin.dhangadhi")
+    login_page.enter_password("password")
+    time.sleep(2)
+    checkbox = driver.find_element(By.CLASS_NAME, value='checkmark')
+    ActionChains(driver).move_to_element(checkbox).click().perform()
+    login_page.click_login()
+    time.sleep(5)
+
+def test_space(setup):
+    login_page = LoginPage(driver)
+    login_page.open_page(URLConfig.LOGIN_PAGE_URL)
+    login_page.enter_username(" ")
+    login_page.enter_password(" ")
+    time.sleep(2)
+    login_page.click_login()
+    time.sleep(5)
+
+def test_blankcred(setup):
+    login_page = LoginPage(driver)
+    login_page.open_page(URLConfig.LOGIN_PAGE_URL)
+    login_page.enter_username("")
+    login_page.enter_password("")
+    time.sleep(2)
+    login_page.click_login()
+    time.sleep(5)
 
